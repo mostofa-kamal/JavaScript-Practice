c275adc2e98fc2782f3bb621a49ba04e5b13411b
mostofa-kamal <mostofa.tgc60@gmail.com>
2025-01-07 00:25:24 +0600

    practive

---
+++ basic_1.js
@@ -0,0 +1,32 @@
+    
+    // variable
+
+    // string variable
+    let name = "tahidur rahman";
+    let email = "tahidur.cse@gmail.com";
+    
+    
+    // number variable
+    var age = 28
+    var Age = 28
+
+
+    // boolean variable
+    let isMarried = true;
+
+    // print alert in browser
+    // alert(name);
+    
+    // print in web page
+    document.write("Name: mostofa  <br>");
+    document.write("Email: tahidur.cse@gmail.com <br>");
+    
+    // print in console
+    console.log(age);
+    
+    
+    // check data type
+     console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof isMarried);
+   
---
+++ conversion.js
@@ -0,0 +1,61 @@
+    // Change from one data type to another
+
+    // From string to number
+    // let age = "28";
+    // let name = "tuhin";
+    // let salary = "Fourty thousand";
+    let price = "12.99";
+
+    // this is string
+    // console.log(typeof age)
+
+
+    // convert string to number using Number
+    // age = Number(age);
+    // salary = Number(salary|0);
+
+    // convert string to number using parseInt
+    // salary = parseInt(salary|0);
+
+
+    // parceFloat will keep the floating point (.99) also
+    // price = parseFloat(price);
+    price = Number(price);
+    
+
+
+
+
+
+
+    // this is number
+    // console.log(typeof age)
+    // console.log(salary, typeof salary)
+    // console.log(typeof salary)
+    console.log(price, typeof price)
+    
+
+    // console.log(typeof name)
+    // console.log(name)
+
+
+//////////////////////////////////////////////////
+    // Number to String conversion
+
+      /* 
+  
+    // Declare a variable as a number
+    let myAge = 28;
+
+
+    console.log(myAge, typeof myAge)
+
+    // Convert a number to a string
+    // myAge = myAge.toString();
+    myAge = String(myAge);
+
+    console.log(myAge, typeof myAge)
+
+    /* 
+        Note: If we try to convert a string to a number, if it is not a number, it will return NaN (Not a Number)
+    */
