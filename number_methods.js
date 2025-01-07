ecd9dfc7e760a05a8b854bb02ff41ee764377cc2
mostofa-kamal <mostofa.tgc60@gmail.com>
2025-01-07 23:27:37 +0600

    number methods

---
+++ number_methods.js
@@ -0,0 +1,19 @@
+// typpeof number
+
+// var num =(20);
+
+// // num = toString(num);
+// num = String(num);
+
+// console.log(typeof (num));
+
+var number = 2.56789;
+
+// shows only 2 decimal points
+console.log(number.toFixed(2));
+
+// shows only 2 digits
+console.log(number.toPrecision(2));
+
+
+// console.log(typeof number);
